ab5fe227124c2318faabd28ba3bbbe7c9650942f
mohamed mohy <es-mohamed.mohy2019@alexu.edu.eg>
2023-02-04 19:38:42 +0200

    adding comments

--- main.py
+++ main.py
@@ -2,34 +2,41 @@ import turtle
 import pandas as pd
 from text_write import TextOnMap
 
+# Screen setup
 screen = turtle.Screen()
 screen.title("Arab Countries Game")
-img = "arab_map.gif"
+img = "arab_map.gif"  # importing image
 screen.addshape(img)
 turtle.shape(img)
 
-text = TextOnMap()
+text = TextOnMap()  # making an object to write on screen
+# choose language
 lang_prompt = screen.textinput(title="Language/اللغة",prompt="Enter 'english' for english language\nاكتب 'العربية' للغة العربية ").lower()
 
 if lang_prompt == "english":  # English
+    # ask user to enter a country name
     answer = screen.textinput(title="Country Name", prompt="Enter a country name\nWrite 'Exit' to quit").title()
-
+    # importing countries data
     data = pd.read_csv('c_en.csv')
-    country_list = data["country"].to_list()
-    print(country_list)
-    inserted_countries = []
+    country_list = data["country"].to_list()  # making a list of country names
+
+    inserted_countries = []  # track the entered countries by user
+
     while answer != "Exit":
-        if answer in country_list:
+        if answer in country_list:  # checking if the user entered a country in the dataset
+            # if the user answer correct extract that country info
             country_info = data[data.country == answer]
+            # write country name on its location on map
             text.write_name(name=answer, x_position=country_info.x, y_position=country_info.y)
-            inserted_countries.append(answer)
+            inserted_countries.append(answer)  # add the answer to the inserted_countries list
+        # prompt the user again
         answer = screen.textinput(title=f"{len(inserted_countries)}/22 country", prompt="Enter a country name").title()
 
-        if len(inserted_countries) >= 22:
+        if len(inserted_countries) >= 22:  # when user enters all countries >> exit
             break
 
 
-else:  # Arabic
+else:  # Arabic  # Default mode
     answer = screen.textinput(title="اسم الدولة", prompt="اكتب اسم دولة\nاكتب 'خروج' لمغادرة البرنامج").title()
 
     data = pd.read_csv('c_ar.csv')
@@ -46,9 +53,9 @@ else:  # Arabic
         if len(inserted_countries) >= 22:
             break
 
-if len(inserted_countries) < 22:
-    countries_missed = list(set(country_list) - set(inserted_countries))
-    pd.DataFrame(countries_missed).to_csv("missed countries.csv")
+if len(inserted_countries) < 22:  # if user failed to enter all the 22 countries
+    countries_missed = list(set(country_list) - set(inserted_countries))       # get the missed countries
+    pd.DataFrame(countries_missed).to_csv("missed countries.csv")  # put them in a csv file
 
 
 
--- text_write.py
+++ text_write.py
@@ -1,16 +1,18 @@
 from turtle import Turtle
 
+# text style
 ALIGN = "center"
 FONT = ('arial', 10, 'normal')
 
 
-class TextOnMap(Turtle):
+class TextOnMap(Turtle):  # a class to create turtle objects to write on screen
     def __init__(self):
         super().__init__()
         self.hideturtle()
         self.penup()
         self.speed("fastest")
 
-    def write_name(self, name, x_position, y_position):
+    def write_name(self, name, x_position, y_position):  # parameters: name, x-cor, y-cor of the country
+        """ Go to the required location and write the name on screen """
         self.goto(int(x_position), int(y_position))
         self.write(arg=name, align=ALIGN, font=FONT)
